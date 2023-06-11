280a7fe9a5273c7fd01d04f7a7c3d560d8a9feff
Ankush Bahekar <ankushmb9@gmail.com>
2023-06-11 23:20:58 +0530

    [Ankush] Add:Checked Employee is Present or Absent

---
+++ EmployeeWageComputationProblem/EmployeeWageComputationProblem/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace EmployeeWageComputationProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom to the Employee Wage Problem");
+            EmpWage employee = new EmpWage();
+            employee.EmployeeAttendance();
+        }
+    }
+}
